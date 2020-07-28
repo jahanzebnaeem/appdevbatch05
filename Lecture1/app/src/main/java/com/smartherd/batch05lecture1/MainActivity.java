16999f1fe430c1a107adb495ba5c9d82c4797d5e
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-07-28 16:04:05 +0500

    Lecture 1
    Installation, first look at android studio and first project.

---
+++ Lecture1/app/src/main/java/com/smartherd/batch05lecture1/MainActivity.java
@@ -0,0 +1,15 @@
+package com.smartherd.batch05lecture1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // TODO: Change this to a new activity.
+    }
+}
