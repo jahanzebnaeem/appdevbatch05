855d660a6bf22bcb54202774dfc70986acc0ac2e
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-08-25 15:21:39 +0500

    Lecture 8
    Buit a project and added layouts and java files to understand fragments.

---
+++ Lecture8/app/src/main/java/com/example/android/fragmentexample/BottomViewFragment.java
@@ -0,0 +1,15 @@
+package com.example.android.fragmentexample;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class BottomViewFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.bottom_view_fragment, container, false);
+    }
+}
