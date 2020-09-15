d049a9b3e339a9e111bea35c4d624e9b51e1de3a
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-09-15 16:07:44 +0500

    Lecture 14
    Setting up firebase database, connecting to firebase database, inserting and displaying data.

---
+++ Lecture14/firebasertdbArtist/app/src/main/java/com/example/android/firebasertdbartist/Artist.java
@@ -0,0 +1,39 @@
+package com.example.android.firebasertdbartist;
+
+public class Artist {
+    String artistId;
+    String artistName;
+    String artistGenre;
+
+    public Artist () {}
+
+    public Artist(String artistId, String artistName, String artistGenre) {
+        this.artistId = artistId;
+        this.artistName = artistName;
+        this.artistGenre = artistGenre;
+    }
+
+    public String getArtistId() {
+        return artistId;
+    }
+
+    public void setArtistId(String artistId) {
+        this.artistId = artistId;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getArtistGenre() {
+        return artistGenre;
+    }
+
+    public void setArtistGenre(String artistGenre) {
+        this.artistGenre = artistGenre;
+    }
+}
--- Lecture14/firebasertdbArtist/app/src/main/java/com/example/android/firebasertdbartist/MainActivity.java
+++ Lecture14/firebasertdbArtist/app/src/main/java/com/example/android/firebasertdbartist/MainActivity.java
@@ -1,14 +1,99 @@
 package com.example.android.firebasertdbartist;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextName;
+    Button buttonAddArtist;
+    Spinner spinnerGenres;
+
+    DatabaseReference databaseArtist;
+
+    ListView listViewArtist;
+    List<Artist> artistList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextName = findViewById(R.id.editTextName);
+        buttonAddArtist = findViewById(R.id.buttonAddArtist);
+        spinnerGenres = findViewById(R.id.spinnerGenres);
+
+        databaseArtist = FirebaseDatabase.getInstance().getReference("artists");
+
+        listViewArtist = findViewById(R.id.listViewArtists);
+
+        artistList = new ArrayList<>();
+        buttonAddArtist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addArtist();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        databaseArtist.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                artistList.clear();
+
+                for(DataSnapshot artistSnapshot: snapshot.getChildren()) {
+                    Artist artist = artistSnapshot.getValue(Artist.class);
+
+                    artistList.add(artist);
+                }
+
+                ArtistList adapter = new ArtistList(MainActivity.this, artistList);
+               listViewArtist.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+
+    private void addArtist() {
+        String name = editTextName.getText().toString().trim();
+        String genre = spinnerGenres.getSelectedItem().toString();
+
+        if(!TextUtils.isEmpty(name)){
+            String id = databaseArtist.push().getKey();
+
+            Artist artist = new Artist(id, name, genre);
+            databaseArtist.child(id).setValue(artist);
+
+            editTextName.setText("");
+            Toast.makeText(this, "Artist Added Successfully", Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, "You should enter a name", Toast.LENGTH_LONG).show();
+        }
     }
 }
