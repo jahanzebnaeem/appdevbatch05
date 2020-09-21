24c500f49e42c9bdc10681bc73306893c03ae05b
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2020-09-21 14:07:51 +0500

    Lecture 15
    Initial checkin

---
+++ Lecture15/firebasertdbArtist/app/src/main/java/com/example/android/firebasertdbartist/ArtistList.java
@@ -0,0 +1,42 @@
+package com.example.android.firebasertdbartist;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class ArtistList extends ArrayAdapter<Artist> {
+    private Activity context;
+    private List<Artist> artistList;
+
+    public ArtistList (Activity context, List<Artist> artistList) {
+        super(context, R.layout.list_layout, artistList);
+        this.context = context;
+        this.artistList = artistList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+
+        View listViewItem = inflater.inflate(R.layout.list_layout, null, true);
+
+        TextView textViewName = listViewItem.findViewById(R.id.textViewName);
+        TextView textViewGenre = listViewItem.findViewById(R.id.textViewGenre);
+
+        Artist artist = artistList.get(position);
+
+        textViewName.setText(artist.getArtistName());
+        textViewGenre.setText(artist.getArtistGenre());
+
+        return listViewItem;
+    }
+}
